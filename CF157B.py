0dd7ccbe6b686aa6b871003a437d24eb34ac3727
SmallSir <280690956@qq.com>
2018-01-17 23:23:00 +0800

    日常

---
+++ CF157B.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = list(map(int,input().split()))
+a.append(0)
+a.sort(reverse=True)
+sum = 0
+for i in range(n):
+    if i % 2 == 0:
+        sum += a[i]*a[i]-a[i+1]*a[i+1]
+print(3.1415926536 * sum)
+
+
+
--- CF293A.py
+++ CF293A.py
@@ -2,6 +2,13 @@ import string
 str1 = input()
 str2 = input()
 n=len(str1)
-print(n)
+flag = 0
+str1 = str1[:n-1] + chr(ord(str1[n-1])+1)
 for i in range(n):
-    print()
+    if str1[n-i-1] > 'z' :
+        str1 = str1[:n-i-1]+ 'a'+str1[n-i:]
+        str1=  str1[:n-i-2]+chr(ord(str1[n-i-2])+1)+str1[n-i-1:]
+if str1 < str2 and flag == 0:
+    print(str1)
+else:
+    print("No such string")
